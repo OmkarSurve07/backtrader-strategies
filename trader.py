23d49ca28cbce21784d162c55748c97f43c4718f
OmkarSurve07 <78922390+OmkarSurve07@users.noreply.github.com>
2023-05-13 12:48:49 +0530

    ...

--- trader.py
+++ trader.py
@@ -2,6 +2,10 @@ import backtrader as bt
 
 cerebro = bt.Cerebro()
 
+cerebro.broker.setcash(1000000.0)
+
 print("Start Portfolio Value: %.2f" % cerebro.broker.getvalue())
+
 cerebro.run()
+
 print("End Portfolio Value: %.2f" % cerebro.broker.getvalue())
